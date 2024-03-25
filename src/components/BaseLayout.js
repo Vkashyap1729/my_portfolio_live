add5d47bb632334bc73de564d7c45e28d16f6dcc
kashyap vishnudas <kashyapvishnudas@gmail.com>
2024-03-25 23:35:18 +0530

    contact me added

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -3,6 +3,7 @@ import Style from './BaseLayout.module.scss';
 import Navbar from './Navbar';
 import Home from './home/Home';
 import About from './about/About';
+import Contact from './contact/Contact';
 import Portfolio from './portfolio/Portfolio';
 import { Route, Routes } from 'react-router-dom';
 import { Box, Grid } from '@mui/material';
@@ -45,6 +46,7 @@ export default function BaseLayout() {
             <Route exact path={'/'} element={<Home />} />
             <Route exact path={'/about'} element={<About />} />
             <Route exact path={'/portfolio'} element={<Portfolio />} />
+            <Route exact path={'/contact'} element={<Contact />} />
           </Routes>
         </Grid>
         <Grid item>
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,5 @@
+import { Box } from '@mui/material';
+const Contact = () => {
+  return <Box>Contact</Box>;
+};
+export default Contact;
